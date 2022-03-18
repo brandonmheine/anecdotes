320c4b50088c1afd4b92b157a6ac4965bb8c27c4
Brandon Heine <brandonmheine@gmail.com>
2022-03-18 11:27:00 -0700

    Finished anecdotes kinda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+]
+
+const Button = ({ onClick, text }) => (
+	<button onClick={onClick}>
+		{text}
+	</button>
+)
+
+const getRandom = () => {
+  const random = Math.floor(Math.random() * anecdotes.length)
+  console.log(random)
+  return random
 }
 
-export default App;
+
+const App = () => {
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const newSelected = () => setSelected(getRandom)
+
+  const handleVoteClick = () => {
+    const vote = [...votes]
+    vote[selected] += 1
+    setVotes(vote)
+  }
+
+  const mostVotes = () => {
+    let max = 0
+    let indexMax = 0
+    votes.forEach((item, index) => {
+      if (item > max) {
+        max = item
+        indexMax = index
+      }
+    })
+    return indexMax
+  }
+
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button text='Vote' onClick={handleVoteClick} />
+      <Button text='Next anecdote' onClick={newSelected} />
+      <h1>Anecdote with most votes</h1>
+      {votes[mostVotes()] === 0 ?
+        <>
+          <p>There are no votes</p>
+        </>
+        :
+        <>
+          <p>{anecdotes[mostVotes()]}</p>
+          <p>has {votes[mostVotes()]} votes</p>
+        </>
+      }
+    </div>
+  )
+}
+
+export default App
